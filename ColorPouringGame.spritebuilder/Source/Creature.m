1aa88d39c63f57591371bbd5f84a7096bb88cae1
vincentwan <xinwan1992@gmail.com>
2015-02-22 15:00:58 -0800

    Automatically Committed for MakeGamesWithUs

--- ColorPouringGame.spritebuilder/Source/Creature.m
+++ ColorPouringGame.spritebuilder/Source/Creature.m
@@ -12,7 +12,7 @@
 
 - (instancetype)initCreature {
     // since we made Creature inherit from CCSprite, 'super' below refers to CCSprite
-    self = [super initWithImageNamed:@"ColorPouringAssets/Assets/bubble.png"];
+    self = [super initWithImageNamed:@"ColorPouringAssets/Assets/20.png"];
     
     if (self) {
         self.isAlive = NO;
