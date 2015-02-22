8b57684908ce1cc558b8de613a480db1e3819035
vincentwan <xinwan1992@gmail.com>
2015-02-22 01:43:22 -0800

    First commit

---
+++ ColorPouringGame.spritebuilder/Source/Grid.h
@@ -0,0 +1,20 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by 万欣 on 15/1/28.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
+
+@end
