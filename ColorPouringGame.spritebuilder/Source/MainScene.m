62aa4982a5c804c4ab2fbdda8087f4451b41db6f
vincentwan <xinwan1992@gmail.com>
2015-02-22 21:04:39 -0800

    Automatically Committed for MakeGamesWithUs

--- ColorPouringGame.spritebuilder/Source/MainScene.m
+++ ColorPouringGame.spritebuilder/Source/MainScene.m
@@ -46,34 +46,34 @@ static UIColor * currColor;
 
 - (void)selectred
 {
-    /*
+    
     currColor = [UIColor colorWithRed:255.0f/255.0f
                     green:0.0f/255.0f
                     blue:0.0f/255.0f
                     alpha:1.0f];
-     */
+    
     NSLog(@"Here red!");
 }
 
 - (void)selectyellow
 {
-    /*
+    
     currColor = [UIColor colorWithRed:255.0f/255.0f
                     green:255.0f/255.0f
                     blue:0.0f/255.0f
                     alpha:1.0f];
-     */
+    
     NSLog(@"Here yellow!");
 }
 
 - (void)selectblue
 {
-    /*
+    
     currColor = [UIColor colorWithRed:0.0f/255.0f
                     green:0.0f/255.0f
                     blue:255.0f/255.0f
                     alpha:1.0f];
-     */
+     
     NSLog(@"Here blue!");
 }
 
