bf8ae5be2afed9c86b0c19915cb0fbf9925fa443
vincentwan <xinwan1992@gmail.com>
2015-04-26 16:20:48 -0700

    Automatically Committed for MakeGamesWithUs

--- ColorPouringGame.spritebuilder/Source/ColorButton.m
+++ ColorPouringGame.spritebuilder/Source/ColorButton.m
@@ -40,8 +40,8 @@ static int btnSizeY = 35;
     [self setBackgroundColor:[CCColor colorWithUIColor:col] forState:CCControlStateNormal];
     [self setBackgroundColor:[CCColor colorWithUIColor:col] forState:CCControlStateSelected];
     [self setBackgroundColor:[CCColor colorWithUIColor:col] forState:CCControlStateHighlighted];
-    //[self setBackgroundOpacity:0.7 forState:CCControlStateSelected];
-    [self setBackgroundOpacity:0.7 forState:CCControlStateHighlighted];
+    [self setBackgroundOpacity:0.7 forState:CCControlStateSelected];
+    //[self setBackgroundOpacity:0.7 forState:CCControlStateHighlighted];
     
     //NSLog(@"Here! setCcolor");
     
