1674e43f130ef5d34b5f29cf10787923b656bfe7
vincentwan <xinwan1992@gmail.com>
2015-04-27 21:28:05 -0700

    Automatically Committed for MakeGamesWithUs

--- ColorPouringGame.spritebuilder/Source/ColorHelper.m
+++ ColorPouringGame.spritebuilder/Source/ColorHelper.m
@@ -13,6 +13,43 @@
 + (UIColor *)setColorWithFile:(int)serialnum {
     UIColor * tempc;
     if(serialnum==0) {
+        tempc=[UIColor colorWithRed:0xFF/255.0f green:0x51/255.0f blue:0x57/255.0f alpha:1.0f];
+    }
+    else if(serialnum==1) {
+        tempc=[UIColor colorWithRed:0xFF/255.0f green:0x79/255.0f blue:0xB7/255.0f alpha:1.0f];
+    }
+    else if(serialnum==2) {
+        tempc=[UIColor colorWithRed:0xC9/255.0f green:0x22/255.0f blue:0xDC/255.0f alpha:1.0f];
+    }
+    else if(serialnum==3) {
+        tempc=[UIColor colorWithRed:0x9A/255.0f green:0x50/255.0f blue:0xDC/255.0f alpha:1.0f];
+    }
+    else if(serialnum==4) {
+        tempc=[UIColor colorWithRed:0x0/255.0f green:0x0/255.0f blue:0xFF/255.0f alpha:1.0f];
+    }
+    else if(serialnum==5) {
+        tempc=[UIColor colorWithRed:0x47/255.0f green:0xF5/255.0f blue:0xFF/255.0f alpha:1.0f];
+    }
+    else if(serialnum==6) {
+        tempc=[UIColor colorWithRed:0x0/255.0f green:0xFF/255.0f blue:0x0/255.0f alpha:1.0f];
+    }
+    else if(serialnum==7) {
+        tempc=[UIColor colorWithRed:0xD7/255.0f green:0xFF/255.0f blue:0x4F/255.0f alpha:1.0f];
+    }
+    else if(serialnum==8) {
+        tempc=[UIColor colorWithRed:0xFF/255.0f green:0xFF/255.0f blue:0x0/255.0f alpha:1.0f];
+    }
+    else if(serialnum==9) {
+        tempc=[UIColor colorWithRed:0xFF/255.0f green:0xBF/255.0f blue:0x0/255.0f alpha:1.0f];
+    }
+    else if(serialnum==10) {
+        tempc=[UIColor colorWithRed:0xFF/255.0f green:0x80/255.0f blue:0x0/255.0f alpha:1.0f];
+    }
+    else {
+        tempc=[UIColor colorWithRed:0xFF/255.0f green:0x65/255.0f blue:0x40/255.0f alpha:1.0f];
+    }
+    /* Color Past
+    if(serialnum==0) {
         tempc=[UIColor colorWithRed:0xFF/255.0f green:0x0/255.0f blue:0x0/255.0f alpha:1.0f];
     }
     else if(serialnum==1) {
@@ -48,6 +85,7 @@
     else {
         tempc=[UIColor colorWithRed:0xFF/255.0f green:0x33/255.0f blue:0x0/255.0f alpha:1.0f];
     }
+     */
     UIColor * color = [tempc copy];
     
     CGFloat red = 0.0, green = 0.0, blue = 0.0, alpha = 0.0;
