c91ab71c2b4c2a9c82ad2328e4b452a3d757438b
vincentwan <xinwan1992@gmail.com>
2015-05-02 02:38:09 -0700

    Automatically Committed for MakeGamesWithUs

--- ColorPouringGame.spritebuilder/Source/MainScene.m
+++ ColorPouringGame.spritebuilder/Source/MainScene.m
@@ -216,8 +216,7 @@ static int totalLevel = 12;
     
     if(levelNum>0) {
         NSLog(@"Back Button");
-        _backBtn = [BackButton initBtnwithX:@"Back" Width:width_total*0.097 Height:height_total*0.908];
-        [_backBtn setTarget:self selector:@selector(backLevel)];
+        _backBtn = [[BackButton alloc] initBtnwithX:@"Back" Width:width_total*0.097 Height:height_total*0.908];
         [_backBtn setTarget:self selector:@selector(backLevel)];
         _backBtn.enabled = true;
         [self addChild:_backBtn];
